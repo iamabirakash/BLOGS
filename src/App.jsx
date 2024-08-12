e84a4aedeb67548ab182d766a363649fc61800cf
Abir AKash <abir030204@gmail.com>
2024-08-12 12:56:46 +0530

    Added Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import Admin from './pages/Admin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        {/* dynamic routing */}
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
