a64c4a1a6edc97935d9c8e2e5255c5386f44bb97
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-14 20:32:47 +0530

    Added Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Admin = () => {
   return (
     <div>
-    <h1>Admin</h1>
+      <div>
+        <form action="" className="grid grid-cols-1 gap-3 bg-white w-[80vw] md:w-[20vw] p-3 rounded-lg">
+          <div className="flex flex-col">
+            <label htmlFor="username" className="text-lg font-semibold text-gray-600">
+              Username
+            </label>
+            <input type="text" name="username" id="username" placeholder="Enter your username" required className="rounded-2xl px-3 py-1 text-lg outline-none bg-gray-100"/>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
