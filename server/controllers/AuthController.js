51be7673bf5cdc716bb387d2456248de8fbbdca8
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-15 15:21:06 +0530

    Added CheckAuth in AuthController.js

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -53,4 +53,19 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { signup, login };
+const checkAuth = async (req, res) => {
+    const id = req.id;
+    try {
+      const user = await User.findById(id).select("-password");
+      if (!user) {
+        return res.status(404).json({ success: false, message: "Access Denied" });
+    }
+    return res
+    .status(200)
+    .json({ success: true, message: "User fetched", user });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+module.exports = { signup, login, checkAuth };
