dc1c4d2db3cbe015b4a793ce6a72f4c6c7ec38ee
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-15 14:04:43 +0530

    Added Singup and Login Routes in Routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const { signup, login } = require("./controllers/AuthController");
+
+router.post("/signup", signup);
+router.post("/login", login);
+
 
 module.exports = router;
