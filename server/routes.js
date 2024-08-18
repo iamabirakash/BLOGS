9a897c8a97b6c97ef994809774dbc3b78bcc601c
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-18 13:50:57 +0530

    Added createBlog POST

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const { signup, login, checkAuth } = require("./controllers/AuthController");
+const { createBlog } = require("./controllers/BlogController");
 const verifyToken = require('./middlewares/verifyToken')
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/checkAuth", verifyToken, checkAuth);
+
+router.post("/createBlog", verifyToken, createBlog);
 
 
 module.exports = router;
