a38c9af599b2eb23f62fb5c2af483906de0ca37b
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-18 10:21:50 +0530

    Added till CheckAuth

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ const { signup, login } = require("./controllers/AuthController");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/checkAuth", verifyToken, checkAuth);
 
 
 module.exports = router;
