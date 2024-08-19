c173e1fb3512702db0dd496a928fb8edca857407
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-19 15:07:19 +0530

    Working Blog Section

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,38 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import BlogCard from './BlogCard'
+import BlogContext from '../context/BlogContext';
+import { convertDate } from '../helpers/convertDate';
+import { getBlogs } from '../helpers/getBlogs';
 
 const BlogSection = () => {
+  const { blogs, setBlogs } = useContext(BlogContext);
+
+  useEffect(() => {
+    getBlogs()
+      .then((data) => setBlogs(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="my-20 mx-10 md:mx-auto w-fit flex flex-col md:flex-row gap-3 lg:gap-8 justify-center items-center">
         {/* SECOND BLOG WILL BE HERE */}
         <div className="py-3 flex mb-4 flex-col gap-3 w-fit rounded-2xl shadow-md mx-auto bg-white">
             <h3 className="text-3xl font-bold mx-5">
-              Say Yes to VS Code
+              {blogs[1]?.title.length > 23
+              ? blogs[1]?.title.slice(0, 23) + "..."
+              : blogs[1]?.title}
             </h3>
-            <img src='/Thumb.png' alt="blogs thumbnail" className="md:w-[30vw] rounded-3xl shadow-lg"/>
+            <img
+              src={blogs[1]?.thumbnail}
+              alt="blogs thumbnail"
+              className="md:w-[30vw] rounded-3xl shadow-lg"
+            />
             <div className="flex gap-3 mx-5 py-2">
-                <span className="px-4 py-2 text-gray-600 text-xs md:text-sm bg-white rounded-full font-semibold shadow-md capitalize">
-                  Coding
+              {blogs[1]?.tags.map((tag, i) => (
+                <span
+                  key={i}
+                  className="px-4 py-2 text-gray-600 text-xs md:text-sm bg-white rounded-full font-semibold shadow-md capitalize"
+                >
+                  {tag}
                 </span>
-                <span className="px-4 py-2 text-gray-600 text-xs md:text-sm bg-white rounded-full font-semibold shadow-md capitalize">
-                  Programming
-                </span>
+              ))}
             </div>
             <hr className="mx-5" />
             <div className="mx-5 flex justify-start items-center gap-3">
-              <img src="/Thumb.png" alt="" className="rounded-full w-[40px] h-[40px]"/>
+              <img
+                src="/Thumb.png"
+                alt=""
+                className="rounded-full w-[40px] h-[40px]"
+              />
               <div>
-                <h4 className="font-bold">Abir Akash</h4>
-                <p className="font-bold">Aug 12</p>
+                <h4 className="font-bold">{blogs[1]?.author}</h4>
+                <p className="font-bold">{convertDate(blogs[1]?.createdAt)}</p>
               </div>
             </div>
-        </div>
-        {/* REST ALL BLOGS WILL BE HERE */}
-        <div className="grid gird-cols-1 gap-3 md:h-[80vh] md:overflow-y-scroll md:px-3 md:pb-2 scoll-hide">
-            <BlogCard />
-            <BlogCard />
-            <BlogCard />
-        </div>
+          </div>
+          {/* REST ALL BLOGS WILL BE HERE */}
+      <div className="grid gird-cols-1 gap-3 -mt-52 md:h-[80vh] md:overflow-y-scroll md:px-3 md:pb-2 scoll-hide">
+        {Array.isArray(blogs) && blogs.length > 2 ? (
+        blogs.slice(2).map((blog) => (
+          <BlogCard key={blog._id} {...blog} />
+        ))
+        ) : (
+          <p>No Additional Blogs Available.</p>
+        )}
+      </div>
     </div>
   )
 }
