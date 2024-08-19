066177d6748eb0c3481534c601c6b9ec99462ff9
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-19 15:08:07 +0530

    Working BlogCard and Showing In BlogSection

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { convertDate } from '../helpers/convertDate'
 
-const BlogCard = () => {
+const BlogCard = ({ thumbnail, title, tags, _id, createdAt, author }) => {
   return (
-    <Link to={`blog/${123}`} className="realtive flex flex-col md:flex-row py-2 gap-3 justify-start items-center md:px-5 rounded-3xl shadow-md bg-white w-full h-fit">
-        <img src='/Thumb.png' alt='Thumbnail' className="md:w-[20vw] rounded-2xl md:rounded-3xl"/>
+    <Link to={`blog/${_id}`} className="realtive flex flex-col md:flex-row py-2 gap-3 justify-start items-center md:px-5 rounded-3xl shadow-md bg-white w-full h-fit">
+        <img src={thumbnail} alt='Thumbnail' className="md:w-[20vw] rounded-2xl md:rounded-3xl"/>
         <div className="px-3 md:px-0">
             <h3 className="text-xl font-bold">
-              Mern Stack is Best
+              {title}
             </h3>
             <div className="flex gap-2 py-2 flex-wrap">
-                <span className="px-4 py-2 text-gray-600 text-xs md:text-sm rounded-full font-semibold shadow-md capitalize">
-                    Coding
+              {tags?.map((tag, i) => (
+                <span
+                  key={i}
+                  className="px-4 py-2 text-gray-600 text-xs md:text-sm rounded-full font-semibold shadow-md capitalize"
+                >
+                  {tag}
                 </span>
-                <span className="px-4 py-2 text-gray-600 text-xs md:text-sm rounded-full font-semibold shadow-md capitalize">
-                    Programming
-                </span>
+              ))}
             </div>
             <hr className="my-2" />
             <div className="flex justify-start items-center gap-3">
               <img src="/Thumb.png" alt="logo" className="rounded-full w-[30px] h-[30px]"/>
               <div>
-                <h4 className="font-bold text-sm">Abir</h4>
-                <p className="font-bold text-sm">12 Aug</p>
+                <h4 className="font-bold text-sm">{author}</h4>
+                <p className="font-bold text-sm">{convertDate(createdAt)}</p>
               </div>
             </div>
         </div>
