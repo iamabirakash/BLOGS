aa00c1c0b79f712d0d4cec4fa905c4b076bd6466
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-19 15:05:28 +0530

    Added GetAllBlogs GET /allBlogs

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { signup, login, checkAuth } = require("./controllers/AuthController");
-const { createBlog } = require("./controllers/BlogController");
+const { createBlog, getAllBlogs } = require("./controllers/BlogController");
 const verifyToken = require('./middlewares/verifyToken')
 
 router.post("/signup", signup);
@@ -8,6 +8,6 @@ router.post("/login", login);
 router.get("/checkAuth", verifyToken, checkAuth);
 
 router.post("/createBlog", verifyToken, createBlog);
-
+router.get("/allBlogs", getAllBlogs);
 
 module.exports = router;
