3230790142b888218824438c73d1601831c477ea
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-22 17:56:01 +0530

    Added getBlogById route

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { signup, login, checkAuth } = require("./controllers/AuthController");
-const { createBlog, getAllBlogs } = require("./controllers/BlogController");
+const { createBlog, getAllBlogs, getBlogById } = require("./controllers/BlogController");
 const verifyToken = require('./middlewares/verifyToken')
 
 router.post("/signup", signup);
@@ -9,5 +9,6 @@ router.get("/checkAuth", verifyToken, checkAuth);
 
 router.post("/createBlog", verifyToken, createBlog);
 router.get("/allBlogs", getAllBlogs);
+router.get("/getBlogById/:id", getBlogById);
 
 module.exports = router;
