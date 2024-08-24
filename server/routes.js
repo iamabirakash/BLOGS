a39142717fbf8e8518f4f4dfde8f170b6185e17f
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-08-24 19:28:27 +0530

    Added All Routes For Comment

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { signup, login, checkAuth } = require("./controllers/AuthController");
 const { createBlog, getAllBlogs, getBlogById } = require("./controllers/BlogController");
+const { getComment } = require("./controllers/CommentController");
 const verifyToken = require('./middlewares/verifyToken')
 
 router.post("/signup", signup);
@@ -10,5 +11,10 @@ router.get("/checkAuth", verifyToken, checkAuth);
 router.post("/createBlog", verifyToken, createBlog);
 router.get("/allBlogs", getAllBlogs);
 router.get("/getBlogById/:id", getBlogById);
+
+router.get("/getComments/:id", getComment);
+router.post("/addComment/:id", addComment);
+router.put("/updateComment/:blogId/:commentId", updateComment);
+router.delete("/deleteComment/:blogId/:commentId", deleteComment);
 
 module.exports = router;
